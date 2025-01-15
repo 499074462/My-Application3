c5e708a9a41f526b1e11b7147be6a4690c335c51
文强 <674299693@qq.com>
2025-01-15 17:13:06 +0800

    branch分支 加了 Retrofit RxJava Adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Retrofit RxJava Adapter
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
 }
---
+++ app/src/main/java/com/youshu/Branch1.java
@@ -0,0 +1,4 @@
+package com.youshu;
+
+public class Branch1 {
+}
